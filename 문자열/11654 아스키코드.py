416aade3a1a74eee65cab1059c74ad45a20d238c
Nogal2222 <njh3321@naver.com>
2022-01-25 20:52:24 +0900

    문자열

---
+++ 문자열/11654 아스키코드.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+A = input()
+print(ord(A))
---
+++ 문자열/11720 숫자의 합.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+N = int(input())
+M = input()
+rslt = 0
+
+for i in M:
+    rslt += int(i)
+
+print(rslt)
