7789fe49513ef5cc0453d1ae2a5f85a6280e4605
Nogal2222 <njh3321@naver.com>
2022-01-26 22:31:21 +0900

    문자열2

---
+++ 문자열/2675 문자열 반복.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+''' 내가 짠 코드 결과는 나오는데 틀렸다고 나옴
+T = int(input())
+
+for i in range(T):
+    RS = list(input().split())
+    
+    for j in range(len(RS[1])):
+        print(RS[1][j] * int(RS[0]), end = "")
+    
+    print()"""(수정)결과값에서 한줄 띄워줘야 함"""
+'''
+
+T = int(input())
+
+for i in range(T):
+    R, S = input().split()
+    rslt = ''
+    
+    for j in S:
+        rslt += int(R) * j
+    
+    print(rslt)
