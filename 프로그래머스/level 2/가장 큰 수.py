0c85ba911e71d1c6a7eb1108e93f280345e0b093
Nogal2222 <njh3321@naver.com>
2022-04-14 23:06:49 +0900

    프로그래머스 레벨 2 9

---
+++ 프로그래머스/level 2/가장 큰 수.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    numbers_str = [str(num) for num in numbers]
+    numbers_str.sort(key = lambda num: num*3, reverse = True)
+    
+    answer = str(int(''.join(numbers_str)))
+    
+    return answer
+
+numbers = [3, 30, 34, 5, 9]
+result = solution(numbers)
