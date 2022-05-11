01529028a4a299ba30d73bd792f2bca7acc3f139
Nogal2222 <njh3321@naver.com>
2022-05-11 23:07:26 +0900

    프로그래머스 레벨 3 12

---
+++ 프로그래머스/lever 3/거스름돈.py
@@ -0,0 +1,13 @@
+def solution(n, money):
+    dp = [1] + [0] * n
+    
+    for i in money:
+        for j in range(i, n+1):
+            if j >= i:
+                dp[j] += dp[j - i]
+    
+    return dp[n] % 1000000007
+
+n = 5
+money = [1, 2, 5]
+result = solution(n, money)
