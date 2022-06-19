3b471ba57d9fbf33e14fcfa86ee0d86693cbe062
Nogal2222 <njh3321@naver.com>
2022-06-19 17:34:29 +0900

    SWEA D3 42

---
+++ SW 엑스퍼트 아카데미/D3/최장 공통 부분 수열.py
@@ -0,0 +1,19 @@
+T = int(input())
+answers = [''] * T
+
+for t in range(1, T+1):
+    a, b = input().split()
+    dp = [[0] * (len(a) + 1) for _ in range(len(b) + 1)]
+    
+    for i in range(1, len(dp)):
+        for j in range(1, len(dp[i])):
+            if a[j-1] == b[i-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                
+    answers[t-1] = f'#{t} {dp[-1][-1]}'
+
+for i in answers:
+    print(i)
