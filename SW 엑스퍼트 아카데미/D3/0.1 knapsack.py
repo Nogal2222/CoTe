d0483d8a42431897d2e1a0c6d7391223131a357c
Nogal2222 <njh3321@naver.com>
2022-06-21 21:13:47 +0900

    SWEA D3 43

---
+++ SW 엑스퍼트 아카데미/D3/0.1 knapsack.py
@@ -0,0 +1,27 @@
+T = int(input())
+Ns = []
+for tc in range(T):
+    N, K = map(int, input().split())
+    items = [list(map(int, input().split())) for _ in range(N)]
+    Ns.append((N, K, items))
+
+results = []
+
+for tc in range(T):
+    N, K, items = Ns[tc]
+    dp = [[0] * (K + 1) for _ in range(N + 1)]
+
+    for i in range(1, len(dp)):
+        v, c = items[i - 1]
+        
+        for j in range(1, len(dp[i])):
+            if j >= v:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - v] + c)
+            
+            else:
+                dp[i][j] = dp[i - 1][j]
+
+    results.append(dp[-1][-1])
+
+for tc in range(T):
+    print(f'#{tc + 1} {results[tc]}')
