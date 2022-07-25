52eb8927cd422126aab8ee59e67dd9c1ed6981cf
노준호 <njh3321@naver.com>
2022-07-25 20:19:14 +0900

    변경

--- 브루트 포스/1018 체스판 다시 칠하기.py
+++ 브루트 포스/1018 체스판 다시 칠하기.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 n, m = map(int, input().split())
 board = []
+
 for i in range(n):
     board.append(input())
+
 repair = []
 
 for i in range(n-7):
     for j in range(m-7):
         first_W = 0
         first_B = 0
+        
         for k in range(i, i+8):
             for l in range(j, j+8):
                 if (k + l) % 2 == 0:
@@ -22,6 +25,7 @@ for i in range(n-7):
                         first_W += 1
                     if board[k][l] != 'W':
                         first_B += 1
+        
         repair.append(first_W)
         repair.append(first_B)
         
